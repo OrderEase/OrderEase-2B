eff295cc1fe3f2f9fc7b8cb29d63cb8a791f9a0c
Els-y <344432376@qq.com>
2018-07-01 00:59:49 +0800

    reset config env

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    BASE_URL: '"http://api.orderease.com/api"',
+    BASE_URL: '"http://api.orderease.com/v1/"',
     CUSTOMER_BASE_URL: '"http://customer.orderease.com"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     NODE_ENV: '"production"',
-    BASE_URL: '"http://118.89.16.142:5000/api"',
+    BASE_URL: '"http://api.orderease.com/v1/"',
     CUSTOMER_BASE_URL: '"http://customer.orderease.com"'
 }
